a30ed3aff01a9e3fe97e88595a956de87afac551
Thanthanakorn <98535350+Thanthanakorn@users.noreply.github.com>
2023-07-02 16:02:57 +0700

    Add login function

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_mysqldb import MySQL
 import yaml
@@ -14,13 +14,16 @@ app.config['MYSQL_DB'] = cred['mysql_db']
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
@@ -62,11 +65,44 @@ def register():
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        loginForm = request.form
+        username = loginForm['username']
+        cur = mysql.connection.cursor()
+        queryStatement = f"SELECT * FROM user WHERE username = '{username}'"
+        numRow = cur.execute(queryStatement)
+        if numRow > 0:
+            user = cur.fetchone()
+            if check_password_hash(user['password'], loginForm['password']):
+
+                # Record session information
+                session['login'] = True
+                session['username'] = user['username']
+                session['userroleid'] = str(user['role_id'])
+                session['firstName'] = user['first_name']
+                session['lastName'] = user['last_name']
+                print(session['username'] + " roleid: " + session['userroleid'])
+                flash('Welcome ' + session['firstName'], 'success')
+                # flash("Log In successful",'success')
+                return redirect('/')
+            else:
+                cur.close()
+                flash("Password doesn't not match", 'danger')
+        else:
+            cur.close()
+            flash('User not found', 'danger')
+            return render_template('login.html')
+        cur.close()
+        return redirect('/')
     return render_template('login.html')
+
 
 @app.route('/logout')
 def logout():
     return render_template('logout.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
