6f44508d6d540725ea43a45335c3342aebe2bd9c
Thanthanakorn <98535350+Thanthanakorn@users.noreply.github.com>
2023-07-02 16:07:29 +0700

    Add logout function, and making a new navbar

--- app.py
+++ app.py
@@ -101,7 +101,9 @@ def login():
 
 @app.route('/logout')
 def logout():
-    return render_template('logout.html')
+    session.clear()
+    flash("You have been logged out", 'info')
+    return redirect('/')
 
 
 if __name__ == '__main__':
