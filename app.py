d305bb3fa960beb7a21fbefdfe0f2197837727af
Thanthanakorn <98535350+Thanthanakorn@users.noreply.github.com>
2023-07-02 15:37:36 +0700

    add register page and function

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "Never push this line to github public repo"
 
-@app.route("/")
+@app.route('/')
 def index():
-	return render_template("index.html")
+    return render_template('index.html')
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        userDetails = request.form
+        p1 = userDetails['first_name']
+        p2 = userDetails['last_name']
+        p3 = userDetails['username']
+        p4 = userDetails['email']
+        p5 = userDetails['password']
+        print(p1 + "," + p2 + "," + p3 + "," + p4 + "," + p5)
+        flash("Form Submitted Successfully.")
+        return redirect('/')
+    return render_template('register.html')
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    return render_template('logout.html')
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
